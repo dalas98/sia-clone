4cb631958ab63b1a52e09c48539a8cc66d8328b6
HansJr <dalas98@gmail.com>
2021-12-05 12:52:20 +0700

    Add Chart in admin

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Subject;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
@@ -10,5 +11,17 @@ class DashboardController extends Controller
     public function index()
     {
         return view('admin.dashboard');
+    }
+
+    public function getChart()
+    {
+        $data['xValues'] = Subject::get()->pluck('name');
+        $data['color'] = [];
+        foreach ($data['xValues'] as $key => $value) {
+            $data['color'][] = "rgb(".rand(0,255).",".rand(0,255).",".rand(0,255).")";
+            $data['yValues'][] = rand(0,255);
+        }
+
+        return $data;
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -8,16 +8,62 @@
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged as Admin!') }}
+                    <canvas id="myChart"></canvas>
                 </div>
             </div>
         </div>
     </div>
 </div>
+<script
+  src="https://code.jquery.com/jquery-3.6.0.slim.min.js"
+  integrity="sha256-u7e5khyithlIdTpu22PHhENmPcRdFiHRjhAuHcs05RI="
+  crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
+<script>
+$(document).ready(function() {
+    $.ajax({
+        url: "{{ url('api/chart-data') }}",
+        method: "GET",
+        success: function(data) {
+            var xValues = [];
+            var yValues = [];
+            var barColors = [];
+
+            var dynamicColors = function() {
+                var r = Math.floor(Math.random() * 255);
+                var g = Math.floor(Math.random() * 255);
+                var b = Math.floor(Math.random() * 255);
+                return "rgb(" + r + "," + g + "," + b + ")";
+            };
+
+            for (var i in data.xValues) {
+                xValues.push(data.xValues[i])
+                yValues.push(data.yValues[i])
+                barColors.push(dynamicColors())
+            }
+
+            new Chart('myChart', {
+                type: 'bar',
+                data: {
+                    labels: xValues,
+                    datasets: [{
+                        data: yValues,
+                        backgroundColor: barColors
+                    }]
+                },
+                options: {
+                    legend: {display: false},
+                    title: {
+                        display: true,
+                        text: "Chart Matakuliah"
+                    }
+                }
+            });
+        },
+        error: function(data) {
+            console.log(data);
+        }
+    })
+})
+</script>
 @endsection
